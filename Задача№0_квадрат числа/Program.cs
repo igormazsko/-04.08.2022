4787429d743f0ae016d04d19da6d4b5a1bf0c600
Igor Matsko <igormazsko@mail.ru>
2022-08-07 20:30:57 +0300

    add sqr namber

---
+++ Задача№0_квадрат числа/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+// Задача 0
+Clear();
+Write("Введите число: ");
+int namber=Convert.ToInt32(ReadLine());
+int sqr=namber*namber;
+WriteLine($"Квадрат числа {namber} = {sqr}");
