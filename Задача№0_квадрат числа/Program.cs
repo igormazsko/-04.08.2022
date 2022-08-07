10740ec67a2a8f6a96d7f6208756d37ae7e88d95
Igor Matsko <igormazsko@mail.ru>
2022-08-07 20:43:17 +0300

    add sqr Parse

--- Задача№0_квадрат числа/Program.cs
+++ Задача№0_квадрат числа/Program.cs
@@ -2,6 +2,8 @@
 // Задача 0
 Clear();
 Write("Введите число: ");
-int namber=Convert.ToInt32(ReadLine());
+int namber=int.Parse(ReadLine());
 int sqr=namber*namber;
+int sqr1=Convert.ToInt32(Math.Pow(namber,2));
 WriteLine($"Квадрат числа {namber} = {sqr}");
+WriteLine($"Квадрат числа {namber} = {sqr1}");
