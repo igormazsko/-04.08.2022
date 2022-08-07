8e73bf94ae748aabcc25397a532f9a93cd5a4376
Igor Matsko <igormazsko@mail.ru>
2022-08-07 21:14:32 +0300

    add Task1 Check Sqr

---
+++ Task1_CheckSqr/Program.cs
@@ -0,0 +1,14 @@
+﻿// Зфдфча 1. "Является ли первое число квадратом второго"
+using static System.Console;
+Write("Введите первое число ");
+int namberA = int.Parse(ReadLine());
+Write("Введите второе число ");
+int namberB = int.Parse(ReadLine());
+if(namberA==namberB*namberB)
+{
+    WriteLine("Является");
+}
+else
+{
+    WriteLine("Не является");
+}
